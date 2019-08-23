e681fd48a25bf9d1ecf037b42952be74489aa785
Noam Kfir <noam@boundlesscode.com>
2019-08-24 02:22:10 +0300

    Merge command definitions into package providers

--- src/core/PackageProvider.js
+++ src/core/PackageProvider.js
@@ -1,5 +1,5 @@
 class PackageProvider {
-    constructor({ defaultRegistry }) {
+    constructor({ defaultRegistry } = {}) {
         this.defaultRegistry = defaultRegistry;
         this.maxRetries = 5;
         this.requestTimeout = 30000;
--- src/providers/nuget/NugetPackageProvider.js
+++ src/providers/nuget/NugetPackageProvider.js
@@ -1,9 +1,31 @@
 const PackageProvider = require('../../core/PackageProvider');
+const { requireCommands } = require('../../core/commandInitializer');
 
 class NugetPackageProvider extends PackageProvider {
   constructor() {
     // super('https://api.nuget.org/v3/index.json');
     super({ defaultRegistry: 'https://api.nuget.org/' });
+  }
+
+  get definition() {
+    return {
+      name: 'nuget',
+      description: 'Manage nuget packages',
+      children: [
+        {
+          name: 'download',
+          description: 'Download packages',
+          alias: 'd',
+          children: requireCommands(__dirname, 'download'),
+        },
+        {
+          name: 'publish',
+          description: 'Publish packages',
+          alias: 'p',
+          children: requireCommands(__dirname, 'publish'),
+        },
+      ],
+    }
   }
 
   async getRegistry({ registry }) {
