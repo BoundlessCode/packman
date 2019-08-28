4a32c2f69f2ac471b79cc357899534810e709a2b
Noam Kfir <noam@boundlesscode.com>
2019-08-29 01:03:13 +0300

    Add missing registry option to npm download search command

--- src/providers/npm/download/NpmDownloadSearchCommand.ts
+++ src/providers/npm/download/NpmDownloadSearchCommand.ts
@@ -1,5 +1,5 @@
 import Command, { CommandExecuteOptions } from '../../../core/Command';
-import { globalOptions, commonPackageOptions, forceOption } from '../../../core/commandOptions';
+import { globalOptions, commonPackageOptions, forceOption, registryOption } from '../../../core/commandOptions';
 import { generatePackageJson } from './npm-search';
 import { getPackageJsonDependencies } from '../crawler';
 import { downloadFromIterable } from './downloader';
@@ -20,6 +20,7 @@ export default class NpmDownloadSearchCommand implements Command {
       flags: '<keyword>',
       description: 'download tarballs based on a npm registry search results',
       options: [
+        registryOption,
         forceOption,
         ...commonPackageOptions,
         ...globalOptions,
@@ -28,16 +29,16 @@ export default class NpmDownloadSearchCommand implements Command {
   }
 
   async execute(options: NpmDownloadSearchCommandOptions) {
-    const { keyword, force = false, logger } = options;
+    const { keyword, force = false, registry, logger } = options;
     const packageJson = await generatePackageJson({
       keyword,
-      registry: options.registry,
+      registry,
     });
     const tarballsSet = await getPackageJsonDependencies({
       packageJson,
       devDependencies: options.devDependencies,
       peerDependencies: options.peerDependencies,
-      registry: options.registry,
+      registry,
       logger,
     });
     return downloadFromIterable(tarballsSet, options.directory, { force, logger });
