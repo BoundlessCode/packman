1d3b7d2b229659985198d66df7ddfd920f95478b
Noam Kfir <noam@boundlesscode.com>
2019-08-31 06:37:51 +0300

    Merge readFromFile into fetch

--- src/providers/npm/download/generator.ts
+++ src/providers/npm/download/generator.ts
@@ -1,17 +1,8 @@
-import { EOL as endOfLine } from 'os';
+import { EOL } from 'os';
 import fs from 'fs';
 
-import { LoggerOptions } from '../../../core/logger';
-import { fetch } from '../../../core/fetcher';
+export const endOfLine = EOL;
 
 export function saveToFile(tarballs: string[], outputFile: string) {
     fs.writeFileSync(outputFile, tarballs.join(endOfLine));
-}
-
-type ReadFromFileOptions = LoggerOptions & {
-}
-
-export async function readFromFile(uri: string, { logger }: ReadFromFileOptions): Promise<string[]> {
-    const text = await fetch<any>({ uri, logger });
-    return text.toString().split(endOfLine);
 }
