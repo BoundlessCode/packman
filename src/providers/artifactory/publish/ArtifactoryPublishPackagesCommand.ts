34558f6af5ce2e437b4f31b7669adc503917d534
Noam Kfir <noam@boundlesscode.com>
2020-01-01 17:37:25 +0200

    Move artifactory publisher options definitions to ArtifactoryPublisher

--- src/providers/artifactory/publish/ArtifactoryPublishPackagesCommand.ts
+++ src/providers/artifactory/publish/ArtifactoryPublishPackagesCommand.ts
@@ -1,19 +1,10 @@
 import Command from '../../../core/Command';
-import { GlobalOptions, globalOptions } from '../../../core/commandOptions';
-import { publisherOptions, PublisherOptions } from '../../../core/Publisher';
-import ArtifactoryPublisher from './ArtifactoryPublisher';
+import ArtifactoryPublisher, { ArtifactoryPublisherOptions, artifactoryPublisherOptions } from './ArtifactoryPublisher';
 
 export type ArtifactoryPublishPackagesCommandOptions =
-  GlobalOptions
-  & PublisherOptions
+  ArtifactoryPublisherOptions
   & {
     packagesPath: string
-    server: string
-    repo: string
-    packageType: string
-    apiKey?: string
-    byChecksum?: boolean
-    force?: boolean
   }
 
 export default class ArtifactoryPublishPackagesCommand implements Command {
@@ -23,21 +14,7 @@ export default class ArtifactoryPublishPackagesCommand implements Command {
       flags: '<packagesPath> <server> <repo> <packageType>',
       description: 'use the artifactory api to publish packages at the specified path to the registry',
       options: [
-        // registryOption,
-        {
-          flags: '--api-key <apiKey>',
-          description: 'your API Key, as specified on your user profile page in Artifactory',
-        },
-        {
-          flags: '--by-checksum',
-          description: 'calculate the sha256, sha1, and md5 checksums and use them to deploy to Artifactory'
-        },
-        {
-          flags: '--force',
-          description: 'publish packages even if they have already been published'
-        },
-        ...publisherOptions,
-        ...globalOptions,
+        ...artifactoryPublisherOptions,
       ],
     };
   }
--- src/providers/artifactory/publish/ArtifactoryPublisher.ts
+++ src/providers/artifactory/publish/ArtifactoryPublisher.ts
@@ -3,21 +3,23 @@ import { URL } from 'url';
 import FormData from 'form-data';
 import { createReadStream } from 'graceful-fs';
 
-import { TimeoutOption } from '../../../core/commandOptions';
+import { TimeoutOption, GlobalOptions, globalOptions } from '../../../core/commandOptions';
 import calculateChecksums from '../../../core/crypto/calculateChecksums';
 import { fetch, Headers, URI } from '../../../core/fetcher';
 import { normalizeRootedDirectory } from '../../../core/shell';
-import Publisher, { PublisherOptions, GetPackageFileInfoOptions, PackageVersionExistsOptions } from '../../../core/Publisher';
+import Publisher, { PublisherOptions, publisherOptions, GetPackageFileInfoOptions, PackageVersionExistsOptions } from '../../../core/Publisher';
 import ArtifactoryPackageInfo from '../ArtifactoryPackageInfo';
 
 const PACKAGE_EXTENSION = 'tar.bz2';
 
-type ArtifactoryPublisherOptions =
-  PublisherOptions
+export type ArtifactoryPublisherOptions =
+  GlobalOptions
+  & PublisherOptions
   & TimeoutOption
   & {
     packagesPath: string
     server: string
+    packageType: string
     repo: string
     api?: string
     apiKey?: string
@@ -25,6 +27,24 @@ type ArtifactoryPublisherOptions =
     force?: boolean
   }
 
+export const artifactoryPublisherOptions = [
+  // registryOption,
+  {
+    flags: '--api-key <apiKey>',
+    description: 'your API Key, as specified on your user profile page in Artifactory',
+  },
+  {
+    flags: '--by-checksum',
+    description: 'calculate the sha256, sha1, and md5 checksums and use them to deploy to Artifactory'
+  },
+  {
+    flags: '--force',
+    description: 'publish packages even if they have already been published'
+  },
+  ...publisherOptions,
+  ...globalOptions,
+];
+  
 export default class ArtifactoryPublisher extends Publisher<ArtifactoryPublisherOptions, ArtifactoryPackageInfo> {
   constructor(options: ArtifactoryPublisherOptions) {
     super(options);
